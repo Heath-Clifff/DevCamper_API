ed27a2d9a43d71c75fc81f2cffe55791430a6904
Arun Locham <arlm907204@aol.com>
2021-09-01 17:55:34 +0530

    establish connection to MongoDB and create Model

---
+++ models/Bootcamp.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter the name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name can not exceed the max limit of 50 characters'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please enter a description'],
+    maxlength: [500, 'Description should not exceed 500 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please enter a valid URL with http of https',
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number cannot exceed 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/,
+      'Please enter a valid email',
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please enter the address'],
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    // Array of Strings
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be atleast 1'],
+    max: [10, 'Rating must not be more than 10'],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Load routes
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+// Load routes
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,8 +24,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(
-    `Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
