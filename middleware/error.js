37fc05ee8596ecd68637defcc120e4c51eec0d80
Arun Locham <arlm907204@aol.com>
2021-09-02 14:30:16 +0530

    add slugify and geojson functionality

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,26 @@
+const ErrorResponse = require('../utils/errorResponse');
 const errorHandler = (err, req, res, next) => {
-  // Console Log for dev
-  console.log(err.stack.red);
+  error = { ...err };
 
-  res.status(500).json({
+  error.message = err.message;
+  console.log(err.name);
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  if (err.code === 11000) {
+    const message = 'Duplicate Key Error';
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((err) => err.message);
+    error = new ErrorResponse(message, 400);
+  }
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: error.message || 'Server Error',
   });
 };
 
