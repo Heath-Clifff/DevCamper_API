e6d09c4f9cef2366b1959a6bc7ede795dd1963cd
Arun Locham <arlm907204@aol.com>
2021-09-02 19:36:53 +0530

    advanced filtering, sorting, selecting/projection

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,12 +1,41 @@
 const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const ErrorResponse = require('../utils/errorResponse');
 
 // @desc      Get all Bootcamps
-// @route     /api/v1/bootcamps
+// @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+  let reqQuery = { ...req.query };
+
+  const removeFields = ['select', 'sort'];
+
+  removeFields.forEach((param) => delete req.params[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+
+  queryStr = queryStr.replace(
+    /\b(lte|gte|lt|gt|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  if (req.query.select) {
+    let fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  if (req.query.sort) {
+    let fields = req.query.sort.split(',').join(' ');
+    query = query.sort(fields);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  const bootcamps = await query;
 
   res
     .status(200)
@@ -14,7 +43,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Get single Bootcamp
-// @route     /api/v1/bootcamps/:id
+// @route     GET /api/v1/bootcamps/:id
 // @access    Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
@@ -29,7 +58,7 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Create Bootcamp
-// @route     /api/v1/bootcamps
+// @route     POST /api/v1/bootcamps
 // @access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.create(req.body);
@@ -38,7 +67,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Update Bootcamp
-// @route     /api/v1/bootcamps/:id
+// @route     PUT/api/v1/bootcamps/:id
 // @access    Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
@@ -55,7 +84,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Delete Bootcamp
-// @route     /api/v1/bootcamps/:id
+// @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
@@ -66,4 +95,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc      Get Bootcamp by radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lng/lat with geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lon = loc[0].longitude;
+  const lat = loc[0].latitude;
+
+  // Calc radius in radians
+  // Radius of Earth: 3973 mi / 6378 km
+  const radius = distance / 3973;
+
+  const bootcamps = await Bootcamp.find({
+    location: {
+      $geoWithin: { $centerSphere: [[lon, lat], radius] },
+    },
+  });
+
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
